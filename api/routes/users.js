0e514051dc17d0a8bcd1c56e7ed0b14c96b42e5f
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-08-25 14:59:24 +0200

    user route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,28 @@
 import express from 'express';
+import { createUser, deleteUser, getUser, getUsers, updateUser } from '../controllers/user.js';
+import { verifyToken } from '../utils/verifyToken.js';
 
 
 const router = express.Router();
 
+router.get('/checkauthentication', verifyToken, (req, res, next) => {
+     console.log(req.user);
+     res.send('hello user, your are logged in!');
+});
+
+// CREATE
+router.post('/', createUser);
+
+// DELETE
+router.delete('/:id', deleteUser);
+
+// UPDATE
+router.put('/:id', updateUser)
+
+// GET
+router.get('/:id', getUser)
+
+// GET ALL
+router.get('/', getUsers)
 
 export default router
