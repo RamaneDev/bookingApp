b0aea570098e1c44f05f5a465f42046cfe7bc9c7
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-08-25 18:01:40 +0200

    using middelwares in hotel route

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+import { verifyToken } from '../utils/verifyToken.js';
+import { verifyAdmin } from '../utils/verifyUser.js';
 
 
 
 const router = express.Router();
 
 // CREATE
-router.post('/', createHotel);
+router.post('/',verifyToken, verifyAdmin, createHotel);
 
 // DELETE
-router.delete('/:id', deleteHotel);
+router.delete('/:id', verifyToken, verifyAdmin, deleteHotel);
 
 // UPDATE
-router.put('/:id', updateHotel)
+router.put('/:id', verifyToken, verifyAdmin, updateHotel)
 
 // GET
 router.get('/:id', getHotel)
