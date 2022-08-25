9550ad543ade96078aba7bac06d1d33a13f04478
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-08-25 18:52:32 +0200

    room route & controller

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,8 +1,24 @@
 import express from 'express';
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/room.js';
+import { verifyToken } from '../utils/verifyToken.js';
+import { verifyAdmin } from '../utils/verifyUser.js';
 
 
 const router = express.Router();
 
+// CREATE
+router.post('/:hotelid',verifyToken, verifyAdmin, createRoom);
 
+// DELETE
+router.delete('/:id/:hotelid', verifyToken, verifyAdmin, deleteRoom);
+
+// UPDATE
+router.put('/:id', verifyToken, verifyAdmin, updateRoom)
+
+// GET
+router.get('/:id', getRoom)
+
+// GET ALL
+router.get('/', getRooms)
 
 export default router
