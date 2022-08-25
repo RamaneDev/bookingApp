e269c640b7ae7283c469600912e1a5cad69c837c
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-08-25 10:52:30 +0200

    login functionality

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
+import { createError } from "../utils/error.js";
 
 export const register = async(req,res,next) => {
     try{
@@ -14,5 +15,21 @@ export const register = async(req,res,next) => {
       res.status(200).send('user has been created.')
     }catch(err) {
         next(err)
+    }
+}
+
+export const login = async (req, res, next) => {
+    try {
+        const user = await User.findOne({username:req.body.username})
+        if(!user) return next(createError(404, 'User not found!'))
+
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if(!isPasswordCorrect) return next(createError(400, 'Wrong password or username!'))
+
+        res.status(200).json(user);
+
+
+    } catch (error) {
+       next(error)
     }
 }
