ca9362b3f7e5f07295143702b3c8482a3307965a
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-08-25 17:44:07 +0200

    verifyUser middelwares

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,18 @@
+import { createError } from "./error.js"
+
+
+export const verifyUser = (req, res ,next) => {
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        next()
+    } else {
+       return next(createError(403, 'You are not authorized'));
+    }
+}
+
+export const verifyAdmin = (req, res ,next) => {
+    if(req.user.isAdmin){
+        next()
+    } else {
+       return next(createError(403, 'You are not authorized'));
+    }
+}
