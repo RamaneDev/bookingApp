82f0cb2175a54461209da58a5ac2fff6a92e0e63
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-08-28 14:14:36 +0200

    Search-bar first part

--- booking-react-ui/src/components/header/Header.jsx
+++ booking-react-ui/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './header.css'
-import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
@@ -32,6 +32,23 @@ const Header = () => {
         <h1 className='headerTitle'>A lifetime of discounts? It's Genius.</h1>
         <p className='headerDesc'>Get rewarded for your travels - unlock instant savings of 10% or more with a free Lamabooking account</p>
         <button className='headerBtn'>Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+             <FontAwesomeIcon icon={faBed} className="headerIcon" />
+             <input type="text" className='headerSearchInput' placeholder='Where are you going?'/>
+          </div>
+          <div className="headerSearchItem">
+             <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+             <span className='headerSearchText'>date to date</span>
+          </div>
+          <div className="headerSearchItem">
+             <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+             <span className='headerSearchText'>2 adults 2 children 1 room</span>
+          </div>
+          <div className="headerSearchItem">
+             <button className="headerBtn">Search</button>
+          </div>
+        </div>
       </div>
     </div>
   )
