0e9a0f91c7504350995f67d45d2c1492377f6458
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-09-06 13:26:11 +0200

    reserve Modal

---
+++ booking-react-ui/src/components/reserve/Reserve.jsx
@@ -0,0 +1,113 @@
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { SearchContext } from "../../context/SearchContext";
+import useFetch from "../../hooks/useFetch";
+import "./reserve.css";
+
+const Reserve = ({ setOpen, hotelId }) => {
+  const [selectedRooms, setSelectedRooms] = useState([]);
+  const { data, loading } = useFetch(`/hotels/room/${hotelId}`);
+  const { dates } = useContext(SearchContext);
+  const navigate = useNavigate();
+
+  const handleSelect = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    setSelectedRooms(
+      checked
+        ? [...selectedRooms, value]
+        : selectedRooms.filter((item) => item !== value)
+    );
+  };
+
+
+
+  const getDatesInRange = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const date = new Date(start.getTime());
+    const dates = [];
+    while (date <= end) {
+      dates.push(new Date(date).getTime());
+      date.setDate(date.getDate() + 1);
+    }
+    return dates;
+  };
+
+  const allDates = getDatesInRange(dates[0].startDate, dates[0].endDate);
+
+  const alreadyReserved = (unavailableDate) =>
+    allDates.includes(new Date(unavailableDate).getTime());
+
+  const isAvailable = (roomNumber) => {
+    console.log(roomNumber)
+    const isFound = roomNumber.unavailableDates.some(alreadyReserved);
+    return !isFound;
+  };
+
+  const handleClick = async () => {
+    try {
+      await Promise.all(
+        selectedRooms.map((roomId) => {
+          const res = axios.put(`/rooms/availability/${roomId}`, {
+            dates: allDates,
+          });
+          return res.data;
+        })
+      );
+      setOpen(false);
+      navigate("/");
+    }catch {}
+  };
+
+  return (
+    <div className="reserve">
+      {loading ? (
+        "Loading"
+      ) : (
+        <div className="rContainer">
+          <FontAwesomeIcon
+            icon={faCircleXmark}
+            className="rClose"
+            onClick={() => setOpen(false)}
+          />
+          <span>Select your rooms:</span>
+          {data.map((item) => (
+            <div className="rItem" key={item._id}>
+              <div className="rItemInfo">
+                <div className="rTitle">{item.title}</div>
+                <div className="rDesc">{item.desc}</div>
+                <div className="rMax">
+                  Max people: <b>{item.maxPeople}</b>
+                </div>
+                <div className="rPrice">{item.price}</div>
+              </div>
+              <div className="rSelectRooms">
+                {item.roomNumbers.map((roomNumber) => (
+                  <div className="room" key={roomNumber._id}>
+                    <label>{roomNumber.number}</label>
+                    <input
+                      type="checkbox"
+                      value={roomNumber._id}
+                      onChange={handleSelect}
+                      disabled={!isAvailable(roomNumber)}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+           <button onClick={handleClick} className="rButton">
+             Reserve Now!
+           </button>
+        </div>
+      )}
+
+    </div>
+  );
+};
+
+export default Reserve;
