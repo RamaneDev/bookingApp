b0882d3ec081ede705f437d91970f1d2f4b3a74e
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-09-10 21:49:23 +0200

    upload image for new user or product

--- admin/src/pages/new/New.jsx
+++ admin/src/pages/new/New.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./new.scss";
 
 const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
+
   return (
     <div className="new">
       <Sidebar />
@@ -16,7 +18,8 @@ const New = ({ inputs, title }) => {
         <div className="bottom">
           <div className="left">
             <img
-              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              src={ file ? URL.createObjectURL(file) :
+                "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" }
               alt=""
               className="img"
             />
@@ -28,7 +31,7 @@ const New = ({ inputs, title }) => {
                   Image:
                   <DriveFolderUploadOutlinedIcon className="icon" />
                 </label>
-                <input type="file" id="file" style={{ display: "none" }} />
+                <input type="file" id="file" onChange={e => setFile(e.target.files[0])} style={{ display: "none" }} />
               </div>
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
