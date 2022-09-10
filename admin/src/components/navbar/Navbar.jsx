ca7424bfda7548c88cc4e5c0eeb2396ab0a937fd
Mesri Abderrahmane <abde_mes@yahoo.fr>
2022-09-11 00:19:55 +0200

    using DarkModeContext in navbar comp to toggle mode

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
@@ -7,8 +7,11 @@ import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNone
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import "./navbar.scss";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Navbar = () => {
+
+  const {dispatch} = useContext(DarkModeContext);
   return (
     <div className="navbar">
       <div className="wrapper">
@@ -21,7 +24,7 @@ const Navbar = () => {
             <LanguageOutlinedIcon className="icon" />
             English
           </div>
-          <div className="item">
+          <div className="item" onClick={() => dispatch({type:'TOGGLE'})}>
             <DarkModeOutlinedIcon className="icon" />
           </div>
           <div className="item">
